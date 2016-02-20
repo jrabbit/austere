cbe242da1ab95ab2bc3eff7b59659f196d30a4a0
jrabbit <jackjrabbit@gmail.com>
2016-02-20 10:26:37 -0500

    run the browsers

--- austere.py
+++ austere.py
@@ -1,6 +1,9 @@
-import psutil
 import logging
 import sys
+import subprocess
+
+import psutil
+
 
 def main(*args):
     overlay = [x for x in psutil.process_iter() if "gameoverlayui" in x.name()]
@@ -8,9 +11,12 @@ def main(*args):
         # all of the gameoverlayui's have the pid
         game_pid = int(overlay[0].cmdline()[2])
         logging.info("Detected game {0}", psutil.Process(pid=game_pid).name())
+        subprocess.call(['iceweasel', sys.argv[1]])
+    else:
+        subprocess.call(['x-www-browser', sys.argv[1]])
     logging.debug(args)
     print(args)
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='example.log', level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
     main(sys.argv)
