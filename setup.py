344ade7e0cfbc994c9de55084ea24430a57bccb8
jrabbit <jackjrabbit@gmail.com>
2016-03-19 22:32:03 -0400

    make desktop file work on install

--- setup.py
+++ setup.py
@@ -8,7 +8,11 @@ setup(
     license='GPL v3',
     # long_description="",
     description="a default browser switcher based on material conditions & contexts",
+    data_files=[('share/applications', ['austere.desktop'])],
     scripts=['austere.py'],
+    # entry_points = {
+    #     'console_scripts': ['austere=austere.command_line:main'],
+    # }
     install_requires=["clint==0.5.1", "psutil==4.0.0"],
     classifiers=['Environment :: X11 Applications',
                  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
