1e6c0f72c039c441409477e1e8d0b2c287816b74
Jack Laxson <jackjrabbit@gmail.com>
2019-12-22 11:26:18 -0500

    ass pylintrc, fixups across script

--- austere.py
+++ austere.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""austere a default browser app manager"""
 import json
 import logging
 import sys
@@ -6,7 +7,7 @@ import shlex
 import subprocess
 import os.path
 import platform
-from typing import List
+from typing import Sequence
 from pathlib import Path
 
 import attr
@@ -16,28 +17,33 @@ import click
 if platform.system() == "Windows":
     import winreg
 
+if sys.version_info[0:2] == (3, 8):
+    from importlib.metadata import future_version
+else:
+    future_version = None
+
 logger = logging.getLogger("austere")
 FOLDER = click.get_app_dir("Austere")
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-def main(url) -> None:
+def main(url: str) -> None:
     with open(os.path.join(FOLDER, "config.json"), "r") as j:
         light_browser = json.loads(j.read())['light_browser']
     use_light = False
     # check for steam games running
-    overlay = [x for x in psutil.process_iter() if "gameoverlayui" in x.name()]
+    overlay = [x for x in psutil.process_iter() if "gameoverlayui" in x.name().decode()]
     if len(overlay) > 0:
         # all of the gameoverlayui's have the pid
         game_pid = int(overlay[0].cmdline()[2])
-        logger.info("Detected game %s", psutil.Process(pid=game_pid).name())
+        logger.info("Detected game %s", psutil.Process(pid=game_pid).name().decode())
         use_light = True
     # check for big picture games
     # they're direct descendants of steam
     elif len(overlay) == 0:
         for proc in psutil.process_iter():
             if proc.name() == "steam":
-                z = list(filter(lambda x: x.name() not in ['steamwebhelper', 'steam', 'sh', 'SteamChildMonit'], proc.children(recursive=True)))
+                z = list(filter(lambda x: x.name().decode() not in ['steamwebhelper', 'steam', 'sh', 'SteamChildMonit'], proc.children(recursive=True)))
                 if len(z) == 1:
                     logger.info("Detected game %s", z[0])
                     use_light = True
@@ -47,7 +53,7 @@ def main(url) -> None:
                     logger.error("Found more than one potential game process, this behavior is undefined")
                     logger.info(z)
             elif proc.name() == 'Battle.net.exe':
-                z = list(filter(lambda x: x.name() not in ['Battle.net Helper.exe', 'CrashMailer_64.exe'], proc.children(recursive=True)))
+                z = list(filter(lambda x: x.name().decode() not in ['Battle.net Helper.exe', 'CrashMailer_64.exe'], proc.children(recursive=True)))
                 if len(z) == 1:
                     logger.info("Detected game %s", z[0])
                     use_light = True
@@ -63,44 +69,45 @@ def main(url) -> None:
         if platform.system() == "Windows":
             pass
         else:
-            subprocess.call([light_browser, sys.argv[1]])
+            subprocess.call([light_browser, url])
     else:
         if platform.system() == "Windows":
-            subprocess.call(['powershell.exe', '-Command', 'start {}'.format(sys.argv[1])])
+            # win 7+ assumption ?
+            subprocess.call(['powershell.exe', '-Command', 'start', url])
         else:
-            subprocess.call(['x-www-browser', sys.argv[1]])
+            subprocess.call(['x-www-browser', url])
 
 
 @attr.s
 class WindowsBrowser:
-    name = attr.ib()
-    enum_order = attr.ib()
-    path = attr.ib()
-    def __str__(self):
+    name: str = attr.ib()
+    enum_order: int = attr.ib()
+    path: str = attr.ib()
+
+    def __str__(self) -> str:
         return f"<{self.enum_order}> {self.name} ({self.path})"
 
 # https://stackoverflow.com/questions/31164253/how-to-open-url-in-microsoft-edge-from-the-command-line#31281412
 # https://msdn.microsoft.com/en-us/library/windows/desktop/cc144175(v=vs.85).aspx
 
-def win_default() -> str:
+def win_default():
     # HKEY_LOCAL_MACHINE\SOFTWARE\Clients\StartMenuInternet
     reg_value = winreg.QueryValue(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Clients\StartMenuInternet")
     return reg_value
 
 
-def win_browser_list() -> List[WindowsBrowser]:
+def win_browser_list() -> Sequence[WindowsBrowser]:
     # https://docs.python.org/3.6/library/winreg.html
     # reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
     with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Clients\StartMenuInternet") as key:
-        target_keys, value_keys, last_mod = winreg.QueryInfoKey(key)
+        target_keys, _, _ = winreg.QueryInfoKey(key)
         l = list()
         for i in range(target_keys):
             v = winreg.EnumKey(key, i)
             logger.debug("got value from StartMenuInternet: %s", v)
             # Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Clients\StartMenuInternet\Firefox-308046B0AF4A39CB\shell\open\command
             path = winreg.QueryValue(key, r"{}\shell\open\command".format(v))
-            wb = WindowsBrowser(name=v, enum_order=i, path=path)
-            l.append(wb)
+            l.append(WindowsBrowser(name=v, enum_order=i, path=path))
         # print(key)
         return l
 
@@ -124,10 +131,10 @@ def pick_browser() -> str:
 class LinuxBrowser(WindowsBrowser):
     pass
 
-def linux_browser_list():
+def linux_browser_list() -> Sequence[LinuxBrowser]:
     paths = subprocess.check_output(
-        shlex.split("update-alternatives --list x-www-browser")).split()
-    return [LinuxBrowser(name=p, enum_order=i, path=p) for p, i in enumerate(paths)]
+        shlex.split("update-alternatives --list x-www-browser")).decode().split()
+    return [LinuxBrowser(name=p, enum_order=i, path=p) for i, p in enumerate(paths)]
 #    not_chrome = list(filter(lambda x: "chrome".encode() not in x, browsers))
 
 
@@ -136,7 +143,7 @@ def linux_browser_list():
 @click.option('--debug', default=False, is_flag=True, help="control log level")
 @click.option('--verbose/--silent', default=True, help="control log level")
 @click.pass_context
-def cli_base(ctx, verbose, debug, my_version):
+def cli_base(ctx: click.Context, verbose: bool, debug: bool, my_version: bool) -> None:
     # mk_folder()
     # only the first basicConfig() is respected.
     if debug:
@@ -153,12 +160,12 @@ def cli_base(ctx, verbose, debug, my_version):
 
 
 @click.group()
-def windows_cli():
+def windows_cli() -> None:
     pass
 
 
 @windows_cli.command()
-def win_register():
+def win_register() -> None:
     "need to be admin"
     value_to_put = f'"{sys.executable} austere" "%1"'
     try:
@@ -174,7 +181,7 @@ def win_register():
         with winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, "austere.HTTPS") as kssl:
             winreg.SetValue(kssl, None, winreg.REG_SZ, value_to_put)
 
-    except OSError as e:
+    except OSError:
         logger.exception("we hit a registry issue")
 
 
@@ -203,7 +210,7 @@ def win_user_reg() -> None:
                 with winreg.CreateKey(openkey, "command") as cmdkey:
                     winreg.SetValue(cmdkey, None, winreg.REG_SZ, value_to_put)
         with winreg.CreateKey(start_menu, "DefaultIcon") as reg_icon:
-            winreg.SetValue(reg_icon,None, winreg.REG_SZ, r"C:\Program Files\Mozilla Firefox\firefox.exe,0")
+            winreg.SetValue(reg_icon, None, winreg.REG_SZ, r"C:\Program Files\Mozilla Firefox\firefox.exe,0")
 
     # with winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, r"Software\RegisteredApplications") as reg_apps:
     #     winreg.SetValueEx(reg_apps, "austere_macguffin", None, winreg.REG_SZ, r"Software\Clients\StartMenuInternet\austere_macguffin\Capabilities")
@@ -212,14 +219,17 @@ def win_user_reg() -> None:
 
 
 @cli_base.command()
-def version():
-    ver = "0.2.0"
+def version() -> None:
+    if future_version:
+        ver = future_version("austere")
+    else:
+        ver = "0.3.0"
     print(f"version {ver} austere")
 
 
 @cli_base.command("help")
 @click.pass_context
-def local_help(ctx):
+def local_help(ctx: click.Context) -> None:
     """Show this message and exit."""
     print(ctx.parent.get_help())
 
@@ -237,19 +247,19 @@ def _browser_default():
         return not_chrome[0]
 
 @click.group()
-def linux():
+def linux() -> None:
     pass
 
 
 @linux.command()
 @click.option("--script_name")
 @click.option("--desktop_path")
-def install(desktop_path, script_name):
+def install(desktop_path: str, script_name: str) -> None:
     "create .desktop file for gnome and other DEs"
     if desktop_path:
         f = desktop_path
     else:
-        f = Path("", "austere.desktop")
+        f = Path("/usr/local/share/applications", "austere.desktop")
     if script_name:
         script = script_name
     else:
@@ -270,7 +280,7 @@ MimeType=text/html;text/xml;application/xhtml_xml;image/webp;x-scheme-handler/ht
 
 @cli_base.command("config")
 @click.option('--light-browser', prompt=True, default=_browser_default)
-def config_cmd(light_browser):
+def config_cmd(light_browser: str) -> None:
     """Configure your austere install. Choose a light-weight browser."""
     cfg_path = os.path.join(FOLDER, "config.json")
     try:
@@ -288,14 +298,14 @@ def config_cmd(light_browser):
 
 @cli_base.command()
 @click.argument("URL")
-def run_on_url(url):
+def run_on_url(url: str) -> None:
     "default action?"
     main(url)
 
 # Hide os-dependent commands
 if platform.system() == "Linux":
     cli = click.CommandCollection(sources=[cli_base, linux])()
-elif platform.system == "Windows":
+elif platform.system() == "Windows":
     cli = click.CommandCollection(sources=[cli_base, windows_cli])
 else:
     cli = cli_base
